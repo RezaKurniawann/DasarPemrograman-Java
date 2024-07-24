cb8331cf8e754344285c9527a5f8872fd781b2e3
rezakurniawann <reza.kurniawan@gmail.com>
2024-07-24 12:26:18 +0700

    first commit

---
+++ variabel/variabel.java
@@ -0,0 +1,32 @@
+// variabel adalah tempat untuk menyimpan data
+
+public class variabel {
+    public static void main (String[]args) {
+
+        // Deklarasi dan Inisilaisasi
+
+        /* Deklarasi adalah menyatakan jenis data dan nama variabel
+         * Inisialisasi adalah menetapkan nilai awal ke variabel
+        */
+
+        String title = "Menghitung Luas Persegi Panjang";
+        int p = 10;
+        int l = 5;
+        int persegi_panjang = 1000;
+
+        System.out.println(title);
+        System.out.println("panjang = " + p);
+        System.out.println("lebar = " + l);
+        System.out.println("Luas Persegi Panjang = " + persegi_panjang);
+
+        System.out.println("-------------------------");
+
+        int a = 7;
+ 
+        System.out.println("Nilai a = " + a);
+
+        int b = a;
+
+        System.out.println("Nilai b = " + b); 
+    }
+}
